6286e29793870205c0f5757033572c2aca95d42e
smcmfmf <hk0831bae@gmail.com>
2025-03-31 15:26:21 +0900

    TextView의 속성들을 JAVA에서 설정

---
+++ app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
@@ -0,0 +1,36 @@
+package kr.ac.kopo.basicwidget;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        TextView text1 = findViewById(R.id.text1);
+        TextView text2 = findViewById(R.id.text2);
+        TextView text3 = findViewById(R.id.text3);
+
+        text1.setTextSize(20);
+        text1.setTextColor(Color.MAGENTA);
+        text1.setText("한국폴리텍대학");
+
+        text2.setTextSize(30);
+        text2.setTextColor(Color.BLUE);
+        text2.setText("인공지능소프트웨어과");
+
+        text3.setSingleLine(true);
+        text3.setText("SingleLine 속성SingleLine 속성SingleLine 속성SingleLine 속성SingleLine 속성SingleLine 속성SingleLine 속성SingleLine 속성");
+    }
+}
